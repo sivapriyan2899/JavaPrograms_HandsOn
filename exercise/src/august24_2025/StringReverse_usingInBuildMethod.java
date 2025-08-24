56c8cb9a29f8680360f6199962f2bfcecbc4e41e
sivapriyan2899 <sivapriyan2899@gmail.com>
2025-08-24 08:22:38 +0530

    reversing string using method

---
+++ exercise/src/august24_2025/StringReverse_usingInBuildMethod.java
@@ -0,0 +1,14 @@
+package august24_2025;
+
+public class StringReverse_usingInBuildMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str ="siva";
+		StringBuilder strBuilder = new StringBuilder();
+		strBuilder.append(str);
+		
+		System.out.println(strBuilder.reverse());
+	}
+
+}
