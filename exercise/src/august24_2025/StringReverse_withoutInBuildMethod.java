4fe9c2e1c400784c099d39ba8e8631b6e18125a2
sivapriyan2899 <sivapriyan2899@gmail.com>
2025-08-24 08:27:44 +0530

    string reverse without method

---
+++ exercise/src/august24_2025/StringReverse_withoutInBuildMethod.java
@@ -0,0 +1,19 @@
+package august24_2025;
+
+public class StringReverse_withoutInBuildMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str ="siva";
+		String[] strArray = str.split("");
+		
+		int strLength = strArray.length;
+		
+		for(int i=strLength-1; i>=0; i--) {
+			System.out.print(strArray[i]);
+		}
+		
+
+	}
+
+}
